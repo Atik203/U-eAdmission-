e2888c84864f111a38da54efeca2f0cf06d50675
Atikur Rahaman <atikurrahaman0305@gmail.com>
2025-04-19 20:11:21 +0600

    fix minor error

--- src/main/java/com/ueadmission/Main.java
+++ src/main/java/com/ueadmission/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
@@ -15,14 +16,15 @@ public class Main extends Application {
     public void start(javafx.stage.Stage stage)  throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com.ueadmission/main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load( ), 1280, 720);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 768);
         // set the fullscreen mode
 
         Image icon = new Image(getClass().getResourceAsStream("/com.ueadmission/uiu_logo_update.png"));
         stage.getIcons().add(icon);
-        stage.initStyle(StageStyle.DECORATED);
-
+        stage.setMaximized(true);
         stage.setTitle("UeAdmission - Home");
+        stage.setMinWidth(1280);
+        stage.setMinHeight(768);
         stage.setScene(scene);
         stage.show();
     }
