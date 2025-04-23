0956acd401670f26441399c395c024476cd00480
Atikur Rahaman <atikurrahaman0305@gmail.com>
2025-04-23 20:33:37 +0600

    added about screen

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.u.eadmission.ueadmission {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
-
+    requires javafx.swing;
+    
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
@@ -11,7 +12,12 @@ module com.u.eadmission.ueadmission {
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
     requires atlantafx.base;
+    requires MaterialFX;
+    requires java.desktop;
 
     opens com.ueadmission to javafx.fxml;
+    opens com.ueadmission.about to javafx.fxml;
+    
     exports com.ueadmission;
+    exports com.ueadmission.about;
 }
